cddffe410c29c468698b7ad56c87484968e19a14
Tiziano Pessa <tiziano.pessa@register.it>
2021-11-26 18:16:18 +0100

    refactor: a better factory

--- final/src/main/java/dev/firenze/kata/pub/BeerFactory.java
+++ final/src/main/java/dev/firenze/kata/pub/BeerFactory.java
@@ -1,18 +1,21 @@
 package dev.firenze.kata.pub;
 
+import java.util.Map;
+
 public class BeerFactory {
 
+  private static final Map<String, Class<? extends Beer>> SPECIAL_BEERS = Map.of(
+      "royal ipa", IpaBeer.class,
+      "belgian trappist tripel", TripelBeer.class,
+      "barrel aged barley wine", BarleyWineBeer.class
+  );
+
   public static Beer create(Beer beer) {
-    switch (beer.getName()) {
-      case "royal ipa":
-        return new IpaBeer(beer.getName(), beer.getQuality());
-      case "belgian trappist tripel":
-        return new TripelBeer(beer.getName(), beer.getQuality());
-      case "barrel aged barley wine":
-        return new BarleyWineBeer(beer.getName(), beer.getQuality());
-      default:
-        return new Beer(beer.getName(), beer.getQuality());
+    Class<? extends Beer> beerClass = SPECIAL_BEERS.getOrDefault(beer.getName(), Beer.class);
+    try {
+      return beerClass.getDeclaredConstructor(String.class, int.class).newInstance(beer.getName(), beer.getQuality());
+    } catch (ReflectiveOperationException e) {
+      throw new IllegalArgumentException("Cannot create instance of " + beer.getName() + " beer");
     }
   }
-
 }
